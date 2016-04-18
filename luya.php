e1643433c8b53e7285ad9cc90925dd8ba2375d05
Basil Suter <git@nadar.io>
2016-04-18 15:29:50 +0200

    fixed deployer change dir issue

--- luya.php
+++ luya.php
@@ -8,15 +8,17 @@ task('deploy:luya', function() {
     // go into configs to write the file
 	cd('{{release_path}}/configs');
 	run('echo "<?php return require \''.$file.'.php\';" > server.php');
+	
+	cd('{{release_path}}');
 	// run all basic luya commands
-	run('cd {{release_path}} && ./vendor/bin/luya migrate --interactive=0');
-	run('cd {{release_path}} && ./vendor/bin/luya import');
-	run('cd {{release_path}} && ./vendor/bin/luya health');
+	run('./vendor/bin/luya migrate --interactive=0');
+	run('./vendor/bin/luya import');
+	run('./vendor/bin/luya health');
 	
 	$commands = (has('commands')) ? get('commands') : [];
 	
 	foreach($commands as $cmd) {
-	    run('cd {{release_path}} && ' . $cmd);
+	    run($cmd);
 	}
 });
 
