54c7663e06308b6479490eef37caf27490927212
Basil <git@nadar.io>
2019-07-22 21:12:44 +0200

    fix config

--- luya.php
+++ luya.php
@@ -10,16 +10,16 @@ set('shared_dirs', [
 ]);
 
 task('luya:composerglobal', function() {
-    run('cd {{release_path}} && {{bin/composer}} global require \"fxp/composer-asset-plugin:^1.4.2\" --verbose --prefer-dist --optimize-autoloader --no-progress --no-interaction');
+    run('cd {{release_path}} && {{bin/composer}} global require "fxp/composer-asset-plugin:^1.4.2" --verbose --prefer-dist --optimize-autoloader --no-progress --no-interaction');
 });
 
 task('luya:config', function() {
      // find file name
-     $file = (has('requireConfig')) ? get('requireConfig') : get('hostname');
+     $env = get('stage');
      // go into configs to write the file
      cd('{{release_path}}/configs');
      
-     run('echo "<?php return require \'env-'.$file.'.php\';" > env.php');
+     run('echo "<?php return require \'env-'.$env.'.php\';" > env.php');
      
      run('{{bin/php}} {{release_path}}/vendor/bin/luya migrate --interactive=0');
 });
