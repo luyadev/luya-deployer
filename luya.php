340f3737b77603e77dacb636aa474065f6ee033f
Basil Suter <git@nadar.io>
2020-04-16 17:08:18 +0200

    php binary removed

--- luya.php
+++ luya.php
@@ -56,17 +56,36 @@ task('luya:commands', function() {
 
 /**
  * Override default composer option in order to provide ignore platform reqs flag.
- * 
- * @since 1.0.6
  */
 set('composer_options', function() {
     $args = null;
 
-    if (has(COMPOSER_IGNORE_PLATFORM_REQS)) {
+    if (has(COMPOSER_IGNORE_PLATFORM_REQS) && get(COMPOSER_IGNORE_PLATFORM_REQS)) {
         $args = ' --ignore-platform-reqs';
     }
     
     return '{{composer_action}} --verbose --prefer-dist --no-progress --no-interaction --no-dev --optimize-autoloader --no-suggest' . $args;
+});
+
+/**
+ * Override the behavior of finding the composer binary. 
+ * 
+ * Shared hosting environments might having problem when prefixing the php binary.
+ * 
+ * @see https://github.com/deployphp/deployer/pull/987 Reason why it has been introduced.
+ */
+set('bin/composer', function () {
+    if (commandExist('composer')) {
+        // original: $composer = locateBinaryPath('composer');
+        return locateBinaryPath('composer');
+    }
+
+    if (empty($composer)) {
+        run("cd {{release_path}} && curl -sS https://getcomposer.org/installer | {{bin/php}}");
+        $composer = '{{release_path}}/composer.phar';
+    }
+
+    return '{{bin/php}} ' . $composer;
 });
 
 // before install vendors, install composer global fxp plugin if enabled
